47a2031a989903e17ded7dca2e44cdf1bfac58dd
Franky Wijanarko <frank.jr.619@gmail.com>
2024-07-04 16:45:20 +0700

    fix ios app theme

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,13 +1,10 @@
 import androidx.compose.ui.window.ComposeUIViewController
-import platform.UIKit.UIScreen
-import platform.UIKit.UIUserInterfaceStyle
+import presentation.ui.app.App
 
 fun MainViewController() = ComposeUIViewController {
 
-    val isDarkTheme =
-        UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyle.UIUserInterfaceStyleDark
+//    val isDarkTheme =
+//        UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyle.UIUserInterfaceStyleDark
 
-    App(
-    darkTheme = isDarkTheme,
-    dynamicColor = false
-) }
+    App()
+}
